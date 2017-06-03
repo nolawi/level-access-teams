59ca64ff33219414e539d2ce883df28db23c2f65
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-06-03 16:24:19 -0400

    adding comments to explain p[rocess

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
+// by nolawi petros
+
 {
 
     let tasks = [];
+    //error handling
+    //
     let errors = [];
+    //listen to submit and send value to localhost
     let DOM = document.querySelector.bind(document);
     DOM('body').addEventListener('submit', add);
     DOM('body').addEventListener('click', remove);
@@ -10,9 +15,12 @@
       //console.log(tasks);
         ev.preventDefault();
         if (ev.target.tname.value === '' ) {
+            //checks if there is a value 
             error('cant save empty team member');
             return;
         }
+        //had to look this up.. left some dummy but name is passing 
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift?v=control
         tasks.unshift({
             text: ev.target.tname.value,
             ptext: 423,
@@ -25,19 +33,23 @@
         ev.target.tname.value = '';
     
     }
-
+    // this is just to delete one item 
     function remove(ev) {
         if (ev.target.classList.contains('done')) {
+            //data attribute for this. instead of index or this. This will scale better..
             tasks[ev.target.dataset.id].done = tasks[ev.target.dataset.id].done ? false: true;
         } else if (ev.target.classList.contains('delete')) {
+            //remove it here
             tasks.splice(ev.target.dataset.id, 1);
         }
       render();
     }
-
+    // remember the abstraction.. this way each field will pass in its own text
     function error(text) {
+        // Just learning the stringfy method in ES6
         DOM('.errors').innerHTML = `<div class="alert alert-danger">${text}</div>`;
         setTimeout(() => {
+            //wish I had jquery for this .. had to look this up too
             DOM('.errors div').style.display = 'none';
         }, 3000);
     }
@@ -54,9 +66,11 @@
       let pID =  423;
       let iID =  6957;
       let descr =  "Dummy Description";
+        // I return the whole thing in html. since i dont have state i am just placing default value. 
+        // 
         return `<li  class="list-group-item">
         ${tname}
-<span class="badge badge-primary">${pID}</span><span class="badge badge-danger">${descr}</span>
+<span class="badge badge-primary">${pID}</span><span class="badge badge-danger">${iID}</span>
             <ul><li class="small">${descr}</li></ul>
 
       <button type="button" class="close pull-right" aria-label="Close"><span class="delete" aria-hidden="true" data-id="${id}">&times;</span></button>
